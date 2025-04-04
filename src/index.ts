129cba2e7f439e47a29a57356ed2974b3c2c6fbf
mainak58 <iammainak58@gmail.com>
2025-04-04 12:08:23 +0530

    LangChain with Google's Generative AI

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { eq } from "drizzle-orm";
 import { Todo } from "..";
 import { db } from "../db";
 import { usersTable } from "../db/schema";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage } from "@langchain/core/messages";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function getAllTodos() {
     const todos = db.select().from(usersTable);
@@ -24,3 +29,23 @@ async function searchTodo(t: number) {
 async function deleteTodo(t: number) {
     await db.delete(usersTable).where(eq(usersTable.id, t));
 }
+
+async function generate() {
+    const model = new ChatGoogleGenerativeAI({
+        model: "gemini-2.0-flash",
+        maxOutputTokens: 50,
+        apiKey: process.env.GEMINI_API_KEY,
+    });
+
+    const response = await model.invoke([
+        [
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ],
+        ["human", "I love programming."],
+    ]);
+
+    console.log(response.content);
+}
+
+generate();
